d1adbdedaddc35b172fc254f1a860a10b92edb7c
AntonioKristicevic <kristicevic.antonio@gmail.com>
2018-02-13 08:53:56 +0100

    Recycler view added and wired up

--- app/src/main/java/com/pavisalavisa/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/pavisalavisa/bitsandpizzas/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends Activity {
                 if(fragment instanceof TopFragment){
                     currentPosition=0;
                 }
-                if(fragment instanceof PizzaFragment){
+                if(fragment instanceof PizzaMaterialFragment){
                     currentPosition=1;
                 }
                 if(fragment instanceof PastaFragment){
@@ -159,7 +159,7 @@ public class MainActivity extends Activity {
 
         switch(position){
             case 1:
-                fragment=new PizzaFragment();
+                fragment=new PizzaMaterialFragment();
                 break;
             case 2:
                 fragment=new PastaFragment();
